fbdf81a6ad141eec371bdded6e75111992edcb8e
LovelyDev829 <lovelydeveloper829@gmail.com>
2022-07-12 11:24:16 +0200

    first test

---
+++ app-files/data.js
@@ -0,0 +1,58 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-temp",
+      "name": "temp",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 2.6531207676577235,
+        "pitch": -0.03608605755570693,
+        "fov": 1.3337360203081263
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6903896261678995,
+          "pitch": -0.9183365086029305,
+          "rotation": 0,
+          "target": "0-temp"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0677690065246743,
+          "pitch": -0.11164709121970873,
+          "title": "TV",
+          "text": "That can display some videos for the clients to watch while the meal."
+        },
+        {
+          "yaw": 1.789857790312844,
+          "pitch": 0.4077134958551518,
+          "title": "Tabels",
+          "text": "So that they can eat something on."
+        }
+      ]
+    }
+  ],
+  "name": "temp project with temp image",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
